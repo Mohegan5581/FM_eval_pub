e573a3b8168c6c99e5881e3985887ac2d9927b03
Jason <jv@jv.com>
2016-06-14 20:50:06 -0400

    commit

--- FM_eval/Form1.Designer.cs
+++ FM_eval/Form1.Designer.cs
@@ -28,12 +28,71 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.fileBrowse = new System.Windows.Forms.Button();
+            this.playerFileLabel = new System.Windows.Forms.Label();
+            this.fileTextBox = new System.Windows.Forms.TextBox();
+            this.dataGridView = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // fileBrowse
+            // 
+            this.fileBrowse.Location = new System.Drawing.Point(364, 11);
+            this.fileBrowse.Name = "fileBrowse";
+            this.fileBrowse.Size = new System.Drawing.Size(75, 23);
+            this.fileBrowse.TabIndex = 0;
+            this.fileBrowse.Text = "Browse";
+            this.fileBrowse.UseVisualStyleBackColor = true;
+            this.fileBrowse.Click += new System.EventHandler(this.fileBrowse_Click);
+            // 
+            // playerFileLabel
+            // 
+            this.playerFileLabel.AutoSize = true;
+            this.playerFileLabel.Location = new System.Drawing.Point(12, 17);
+            this.playerFileLabel.Name = "playerFileLabel";
+            this.playerFileLabel.Size = new System.Drawing.Size(88, 13);
+            this.playerFileLabel.TabIndex = 1;
+            this.playerFileLabel.Text = "Player File (CSV):";
+            // 
+            // fileTextBox
+            // 
+            this.fileTextBox.Location = new System.Drawing.Point(106, 14);
+            this.fileTextBox.Name = "fileTextBox";
+            this.fileTextBox.Size = new System.Drawing.Size(252, 20);
+            this.fileTextBox.TabIndex = 2;
+            // 
+            // dataGridView
+            // 
+            this.dataGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView.Location = new System.Drawing.Point(106, 40);
+            this.dataGridView.Name = "dataGridView";
+            this.dataGridView.Size = new System.Drawing.Size(1328, 569);
+            this.dataGridView.TabIndex = 3;
+            this.dataGridView.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(1467, 621);
+            this.Controls.Add(this.dataGridView);
+            this.Controls.Add(this.fileTextBox);
+            this.Controls.Add(this.playerFileLabel);
+            this.Controls.Add(this.fileBrowse);
+            this.Name = "Form1";
+            this.Text = "FM Eval";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button fileBrowse;
+        private System.Windows.Forms.Label playerFileLabel;
+        private System.Windows.Forms.TextBox fileTextBox;
+        private System.Windows.Forms.DataGridView dataGridView;
     }
 }
 
--- FM_eval/Form1.cs
+++ FM_eval/Form1.cs
@@ -8,6 +8,13 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
+// additional code - this helps short cut some of the junk you would have to type if you didnt do it
+// for example: System.Text.RegularExpressions.Regex.IsMatch
+// also as it turns out, you don't need to do this to check for file extensions
+// the OpenFileDialog has it built in
+using System.Text.RegularExpressions;
+using System.Data.OleDb;
+
 namespace FM_eval
 {
     public partial class Form1 : Form
@@ -15,6 +22,49 @@ namespace FM_eval
         public Form1()
         {
             InitializeComponent();
+        }
+
+        public DataTable readCsv(string fileName)
+        {
+            DataTable dt = new DataTable("Data");
+            using (OleDbConnection cn = new OleDbConnection("Provider=Microsoft.Jet.OLEDB.4.0;Data Source=\"" + System.IO.Path.GetDirectoryName(fileName) + "\";Extended Properties='text;HDR=yes;FMT=Delimited(,)';"))
+            {
+                using (OleDbCommand cmd = new OleDbCommand(string.Format("select * from [{0}]", new System.IO.FileInfo(fileName).Name), cn))
+                {
+                    cn.Open();
+                    using(OleDbDataAdapter adapter = new OleDbDataAdapter(cmd))
+                    {
+                        adapter.Fill(dt);
+                    }
+                }
+            }
+            return dt;
+        }
+        OpenFileDialog ofd = new OpenFileDialog();
+
+        private void fileBrowse_Click(object sender, EventArgs e)
+        {
+            ofd.Filter = "CSV|*.csv";
+            // original before I found out about ofd.Filter
+            //string pattern = ".*\\.csv$";
+            //if (ofd.ShowDialog() == DialogResult.OK && Regex.IsMatch(ofd.FileName.Trim(), pattern, RegexOptions.IgnoreCase))
+            if (ofd.ShowDialog() == DialogResult.OK)
+            {
+                //Console.Write(pattern);
+                //Console.Write(ofd);
+                fileTextBox.Text = ofd.FileName;
+                dataGridView.DataSource = readCsv(ofd.FileName);
+            }
+            else
+            {
+                MessageBox.Show("Please select a CSV only", "FM Eval",MessageBoxButtons.OKCancel, MessageBoxIcon.Asterisk);
+            }
+
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
         }
     }
 }
